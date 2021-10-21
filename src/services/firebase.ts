48ccc6d12affa452a4f647f1dc5e14754aaf110f
Marcus <md.daves@gmail.com>
2021-10-21 10:07:22 -0300

    feat: aula 02 - maximum speed

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,8 +1,7 @@
-import {initializeApp} from 'firebase/app';
+import firebase from 'firebase/app';
 
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-
+import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,9 +13,11 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID 
 };
 
+firebase.initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig)
+const auth = firebase.auth();
+const database = firebase.database();
 
-const auth = getAuth(app);
-const database = getDatabase(app);
+export { firebase, auth, database  }
+
 
